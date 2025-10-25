0d28a93bac0877c86f89ce423fafb38a4db8e6fa
AurumDAfpel <santiagogomezrojas2007@gmail.com>
2025-10-25 13:31:50 -0500

    feat: se añadio la ruta del json y la funcion cargar eventos

---
+++ src/listarEventos.py
@@ -0,0 +1,15 @@
+import json
+
+rutaArchivos = "data/eventos.json"
+
+def cargar_eventos(ruta_archivo):
+    try:
+        with open(ruta_archivo, 'r', encoding='utf-8') as archivo:
+            eventos = json.load(archivo)
+        return eventos
+    except FileNotFoundError:
+        print(f"Error: El archivo {ruta_archivo} no se encontró.")
+        return []
+    except json.JSONDecodeError:
+        print(f"Error: El archivo {ruta_archivo} no es un JSON válido.")
+        return []
